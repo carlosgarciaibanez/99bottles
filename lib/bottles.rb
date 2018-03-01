2ba55ed2f43f68b53c5404efca8ebdb05db11acd
cgibanez <cgibanez@tuenti.com>
2018-03-01 07:22:50 +0100

    Added the 'container' method to reduce code duplication

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -18,12 +18,17 @@ class Bottles
       when 1
 	return "1 bottle of beer on the wall, 1 bottle of beer.\n" +
 		"Take it down and pass it around, no more bottles of beer on the wall.\n"
-      when 2
-        return "2 bottles of beer on the wall, 2 bottles of beer.\n" +
-		"Take one down and pass it around, 1 bottle of beer on the wall.\n"
       else
-        return "#{verse_num} bottles of beer on the wall, #{verse_num} bottles of beer.\n" +
-		"Take one down and pass it around, #{verse_num - 1} bottles of beer on the wall.\n"
+        return "#{verse_num} #{container(verse_num)} of beer on the wall, #{verse_num} bottles of beer.\n" +
+		"Take one down and pass it around, #{verse_num - 1} #{container(verse_num - 1)} of beer on the wall.\n"
+     end
+  end 
+
+  def container(num) 
+     if (num == 1)
+        return "bottle"
+     else 
+        return "bottles"
      end
   end 
 end
